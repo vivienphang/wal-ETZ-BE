03cc7663b5837e5efdae46788e988e69f4226a18
floatingtales <58957425+floatingtales@users.noreply.github.com>
2022-08-16 20:24:48 +0700

    changed datatype ts setting

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,10 @@ const app: express.Application = express();
 const PORT: number | string = process.env.PORT || 3030;
 
 const userController: UserController = new UserController(userModel);
-const userRoutes = new UserRoutes(userController, "middleware tba").routes();
+const userRoutes: express.Router = new UserRoutes(
+  userController,
+  "middleware tba"
+).routes();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
