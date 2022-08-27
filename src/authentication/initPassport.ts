bf7ed150bb6fda53901c501e7205aec1ef2fae66
floatingtales <58957425+floatingtales@users.noreply.github.com>
2022-08-27 11:32:07 +0700

    oauth cleanup

--- src/authentication/initPassport.ts
+++ src/authentication/initPassport.ts
@@ -1,19 +1,20 @@
-// eslint-disable-next-line import/no-import-module-exports
+import { Express } from "express";
 import passport from "passport";
-// import googleStrategy from "./googleStrategy";
+import googleStrategy from "./googleStrategy";
 
 const initPassport = (app: any) => {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  passport.serializeUser((userModel, done) => {
+  passport.serializeUser((userModel: Express.User, done) => {
     done(null, userModel);
   });
 
-  passport.deserializeUser((userModel, done) => done(null, userModel));
+  passport.deserializeUser((userModel: Express.User, done) => {
+    done(null, userModel);
+  });
 
-  // eslint-disable-next-line global-require
-  require("./googleStrategy")(passport);
+  googleStrategy(passport);
 };
 
 export default initPassport;
