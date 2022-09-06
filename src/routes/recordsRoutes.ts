42bb57db34b18f96e4b9cef76daeb86842012cd6
Luthor9269 <mayank_aswal@live.com>
2022-09-06 08:10:45 +0700

    Merging

---
+++ src/routes/recordsRoutes.ts
@@ -0,0 +1,13 @@
+import express, { Router } from "express";
+import BaseRoutes from "./baseRoutes";
+
+const router: Router = express.Router();
+
+export default class RecordsRoutes extends BaseRoutes {
+  routes() {
+    /* no auth routes */
+    // for testing purposes
+    router.post("newRecord", this.controller.newRecord.bind(this.controller));
+    return router;
+  }
+}
