a35a2bccefca4c3df722e1963db5817f6f35b351
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2022-09-17 01:06:49 +0800

    camera linked to s3

--- src/controller/recordsController.ts
+++ src/controller/recordsController.ts
@@ -1,11 +1,18 @@
 import { Model } from "mongoose";
 import { Request, Response } from "express";
+import fs from "fs";
+import aws from "aws-sdk";
 import { AccountsAttributes } from "../types/accountsInterface";
 import { RecordsAttributes } from "../types/recordsInterface";
 import BaseController from "./baseController";
 import responseStatus from "./responseStatus";
 
-const { CREATE_RECORD_FAILED, CREATE_ACCOUNT_FAILED } = responseStatus;
+const {
+  CREATE_RECORD_FAILED,
+  CREATE_ACCOUNT_FAILED,
+  UPDATE_PICTURE_FAILED,
+  UPDATE_PICTURE_SUCCESS,
+} = responseStatus;
 
 export default class RecordsController extends BaseController {
   public accounts: Model<AccountsAttributes>;
@@ -74,5 +81,36 @@ export default class RecordsController extends BaseController {
       return res.status(400).json({ status: CREATE_ACCOUNT_FAILED });
     }
     return res.status(200).json({ updateAccount });
+  }
+
+  async addReceiptS3(req: Request, res: Response) {
+    // connecting to S3 bucket
+    const s3 = new aws.S3({
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      region: process.env.S3_BUCKET_REGION,
+    });
+    try {
+      if (req.file === null) {
+        return res.status(400).json({ message: "Please choose another file." });
+      }
+      const { file } = req;
+      const fileStream: any = fs.readFileSync(file.path);
+
+      const uploadedImage = await s3
+        .upload({
+          Bucket: process.env.BUCKET_NAME,
+          Key: file.originalname,
+          Body: fileStream,
+        })
+        .promise();
+      console.log("uploaded image:", uploadedImage);
+
+      return res
+        .status(200)
+        .json({ status: UPDATE_PICTURE_SUCCESS, data: uploadedImage.Location });
+    } catch (err) {
+      return res.status(400).json({ status: UPDATE_PICTURE_FAILED });
+    }
   }
 }
--- src/routes/recordsRoutes.ts
+++ src/routes/recordsRoutes.ts
@@ -1,5 +1,8 @@
 import express, { Router } from "express";
+import multer from "multer";
 import BaseRoutes from "./baseRoutes";
+// Set the name of the upload directory here
+const multerUpload = multer!({ dest: "uploads/" });
 
 const router: Router = express.Router();
 
@@ -9,6 +12,13 @@ export default class RecordsRoutes extends BaseRoutes {
     // for testing purposes
     router.use(this.JWTMiddleware);
     router.post("/newRecord", this.controller.newRecord.bind(this.controller));
+
+    router.post(
+      "/addReceiptS3",
+      multerUpload.single("file"),
+      this.controller.addReceiptS3.bind(this.controller)
+    );
+
     return router;
   }
 }
