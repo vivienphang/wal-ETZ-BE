698ae7be71e72e5377a6409ffc71d27419f33649
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2022-09-19 01:16:58 +0800

    updated usersController

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,31 @@
+// import S3Client from "aws-sdk/clients/s3";
+// import aws from "aws-sdk";
 import express, { Router } from "express";
-
 import multer from "multer";
+// import multerS3 from "multer-s3";
 import BaseRoutes from "./baseRoutes";
 // Set the name of the upload directory here
 const multerUpload = multer!({ dest: "uploads/" });
+// const s3Config = new aws.S3Client({
+//   region: process.env.S3_BUCKET_REGION,
+//   credentials: {
+//     accessKeyId: process.env.S3_ACCESS_KEY as string,
+//     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+//   },
+// });
+
+// const multerUpload = multer({
+//   storage: multerS3({
+//     s3: s3Config,
+//     bucket: "bucket-name",
+//     metadata(req, file, cb) {
+//       cb(null, { fieldName: file.fieldname });
+//     },
+//     key(req, file, cb) {
+//       cb(null, "image.jpeg");
+//     },
+//   }),
+// });
 
 const router: Router = express.Router();
 
@@ -25,9 +47,14 @@ export default class UsersRoutes extends BaseRoutes {
       "/populateRecords",
       this.controller.populateRecords.bind(this.controller)
     );
+
     router.post(
-      "/updateProfile",
-      this.controller.updateProfile.bind(this.controller)
+      "/updateUsernameOnly",
+      this.controller.updateUsernameOnly.bind(this.controller)
+    );
+    router.post(
+      "/updateCurrencyOnly",
+      this.controller.updateCurrencyOnly.bind(this.controller)
     );
     router.post(
       "/updatePicture",
